0294fb3aa57507ad8cfdcfade3a19cb0febbf703
yanggu0t <zxc225751@gmail.com>
2025-03-06 19:03:17 +0800

    feat: adjust search tokenizer for mandarin

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,32 +1,30 @@
-import { source } from '@/lib/source';
-import { createFromSource } from 'fumadocs-core/search/server';
 import { createTokenizer } from '@orama/tokenizers/mandarin';
- 
+import { createFromSource } from 'fumadocs-core/search/server';
+
+import { source } from '@/lib/source';
+
 export const { GET } = createFromSource(source, undefined, {
   localeMap: {
-    // you can customise search configs for specific locales, like:
-    // [locale]: Orama options
- 
-    zhHans: {
+    'zh-hans': {
       components: {
         tokenizer: createTokenizer(),
       },
       search: {
-        threshold: 0,
+        threshold: 500,
         tolerance: 0,
       },
     },
-    
-    zhHant: {
+
+    'zh-hant': {
       components: {
         tokenizer: createTokenizer(),
       },
       search: {
-        threshold: 0,
+        threshold: 500,
         tolerance: 0,
       },
     },
- 
+
     // use the English tokenizer
     en: 'english',
   },
