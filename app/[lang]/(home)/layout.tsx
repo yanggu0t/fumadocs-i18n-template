9e0c022227715ad4472863e890ae22d625d155d1
yanggu0t <zxc225751@gmail.com>
2025-03-06 18:07:34 +0800

    chore: install prettier dependency

--- app/[lang]/(home)/layout.tsx
+++ app/[lang]/(home)/layout.tsx
@@ -1,17 +1,5 @@
-// import { redirect } from "next/navigation"
-
-// export default async function Layout({
-//   params,
-// }: {
-//   params: Promise<{ lang: string }>;
-// }) {
-//   const lang = (await params).lang;
-  
-//   return redirect(`/${lang}/docs`);
-// }
-
 import type { ReactNode } from 'react';
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  return children
+  return children;
 }
